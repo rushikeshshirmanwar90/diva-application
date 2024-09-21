9961a9e8826ffb2b88964fd815f09d6f43a13a5e
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-09-22 00:52:17 +0530

    adding Category Details page

--- components/Navigation/Navigator.tsx
+++ components/Navigation/Navigator.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../../screens/Home";
 import Details from "../../screens/Details";
 import Cart from "../../screens/Cart";
+import CategoryDetail from "../../screens/CategoryDetail";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +13,10 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Cart" component={Cart} />
         <Stack.Screen name="DIVA" component={Home} />
         <Stack.Screen name="Details" component={Details} />
-        <Stack.Screen name="Cart" component={Cart} />
+        <Stack.Screen name="Category Detail" component={CategoryDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
